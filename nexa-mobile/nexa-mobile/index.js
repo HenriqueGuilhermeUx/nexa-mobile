4a134d566200cdd5a095769b4f5d8a83d994deab
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-07-25 02:15:39 -0300

    Attach app version headers and register mandatory update gate

--- nexa-mobile/nexa-mobile/index.js
+++ nexa-mobile/nexa-mobile/index.js
@@ -1,4 +1,27 @@
 import { registerRootComponent } from 'expo';
-import App from './App';
+import ForceUpdateRoot from './ForceUpdateRoot';
 
-registerRootComponent(App);
+const CURRENT_VERSION = '1.4.7';
+const API_HOST = 'nexa-backend-p2u0.onrender.com';
+const nativeFetch = global.fetch;
+
+global.fetch = function nexaFetch(resource, options = {}) {
+  const url = typeof resource === 'string' ? resource : String(resource?.url || '');
+
+  if (!url.includes(API_HOST)) {
+    return nativeFetch(resource, options);
+  }
+
+  const headers = {
+    ...(options.headers || {}),
+    'X-Nexa-App-Version': CURRENT_VERSION,
+    'X-Nexa-Platform': 'android',
+  };
+
+  return nativeFetch(resource, {
+    ...options,
+    headers,
+  });
+};
+
+registerRootComponent(ForceUpdateRoot);
