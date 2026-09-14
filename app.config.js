54a0fb592f189c74fcc0a519912806a7c1f811ca
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-14 17:55:05 -0300

    build(android): bump Nexa 2.0.12 to versionCode 107

--- app.config.js
+++ app.config.js
@@ -3,14 +3,14 @@ module.exports = ({ config }) => ({
   version: '2.0.12',
   ios: {
     ...(config.ios || {}),
-    buildNumber: '106',
+    buildNumber: '107',
   },
   android: {
     ...(config.android || {}),
-    versionCode: 106,
+    versionCode: 107,
   },
   extra: {
     ...(config.extra || {}),
-    releaseBuild: 'android16-api36-2.0.12-v106-production-safe',
+    releaseBuild: 'android16-api36-2.0.12-v107-production-safe',
   },
 });
