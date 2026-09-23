3788fd74f181fa7256632f76adf7ab7443e12d87
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-23 14:36:57 -0300

    chore(android): bump Open Finance refresh fix to 2.0.16 v111

--- app.config.js
+++ app.config.js
@@ -1,16 +1,16 @@
 module.exports = ({ config }) => ({
   ...config,
-  version: '2.0.15',
+  version: '2.0.16',
   ios: {
     ...(config.ios || {}),
-    buildNumber: '110',
+    buildNumber: '111',
   },
   android: {
     ...(config.android || {}),
-    versionCode: 110,
+    versionCode: 111,
   },
   extra: {
     ...(config.extra || {}),
-    releaseBuild: 'android16-api36-2.0.15-v110-assistant-open-finance-recovery-pilot',
+    releaseBuild: 'android16-api36-2.0.16-v111-assistant-open-finance-refresh-fix',
   },
 });
