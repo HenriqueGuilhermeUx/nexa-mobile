033d8f8cf2f1d70f6aa37d049280c821d4b444ac
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-23 15:38:40 -0300

    chore(android): prepare Open Finance production release 2.0.17 v112

--- app.config.js
+++ app.config.js
@@ -1,16 +1,16 @@
 module.exports = ({ config }) => ({
   ...config,
-  version: '2.0.16',
+  version: '2.0.17',
   ios: {
     ...(config.ios || {}),
-    buildNumber: '111',
+    buildNumber: '112',
   },
   android: {
     ...(config.android || {}),
-    versionCode: 111,
+    versionCode: 112,
   },
   extra: {
     ...(config.extra || {}),
-    releaseBuild: 'android16-api36-2.0.16-v111-assistant-open-finance-refresh-fix',
+    releaseBuild: 'android16-api36-2.0.17-v112-open-finance-production',
   },
 });
