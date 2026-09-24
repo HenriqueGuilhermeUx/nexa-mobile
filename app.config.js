cd10b6e095488c0def658344c64afdec9338cec1
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-23 21:52:27 -0300

    chore(release): bump Nexa biometric AAB to 2.0.19 v114

--- app.config.js
+++ app.config.js
@@ -1,16 +1,16 @@
 module.exports = ({ config }) => ({
   ...config,
-  version: '2.0.18',
+  version: '2.0.19',
   ios: {
     ...(config.ios || {}),
-    buildNumber: '113',
+    buildNumber: '114',
   },
   android: {
     ...(config.android || {}),
-    versionCode: 113,
+    versionCode: 114,
   },
   extra: {
     ...(config.extra || {}),
-    releaseBuild: 'android16-api36-2.0.18-v113-open-finance-official',
+    releaseBuild: 'android16-api36-2.0.19-v114-biometric-open-finance',
   },
 });
