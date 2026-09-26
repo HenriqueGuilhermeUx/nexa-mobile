a9aab9305d6ec964cc14ead639a17a01f4f0feae
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-25 21:07:35 -0300

    chore(wallet-first): bump internal pilot to v116

--- app.config.js
+++ app.config.js
@@ -3,14 +3,14 @@ module.exports = ({ config }) => ({
   version: '2.0.20',
   ios: {
     ...(config.ios || {}),
-    buildNumber: '115',
+    buildNumber: '116',
   },
   android: {
     ...(config.android || {}),
-    versionCode: 115,
+    versionCode: 116,
   },
   extra: {
     ...(config.extra || {}),
-    releaseBuild: 'android16-api36-2.0.20-v115-release-ready',
+    releaseBuild: 'android16-api36-2.0.20-v116-wallet-first-internal',
   },
 });
